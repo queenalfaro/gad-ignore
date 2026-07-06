0b86180898dedc1c57594c5eac718b03ea66b1a9
queenalfaro <queenalfaro@op.pl>
2026-07-06 14:39:36 +0500

    add debug

--- index.ts
+++ index.ts
@@ -2,8 +2,10 @@ export default {
 	async fetch(request) {
 		const url = new URL(request.url);
 		const subdomain = url.hostname.split('.')[0];
+		console.log(subdomain)
 		const isG = subdomain in ['g', 'gi', 'git', 'gitignore'];
 		const isD = subdomain in ['d', 'di', 'docker', 'dockerignore'];
+		console.log(isG, isD)
 		if (isG && isD || !isG && !isD) {
 			return new Response("ERROR!!! Couldn't determine the type (G/D).");
 		}
