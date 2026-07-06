f26db781d66b7e8fe53ea11ffc0931b6415f76c3
queenalfaro <queenalfaro@op.pl>
2026-07-06 14:19:19 +0500

    add workers config

--- index.ts
+++ index.ts
@@ -1,17 +1,7 @@
-/**
- * @typedef {Object} Env
- */
-
 export default {
-	/** 
-	 * @param {Request} request
-	 * @param {Env} env
-	 * @param {ExecutionContext} ctx
-	 * @returns {Promise<Response>}
-	 */
 	async fetch(request) {
 		const url = new URL(request.url);
-		const response = await fetch(`http://gitignore.io/api/${url.pathname}`);
+		const response = await fetch(`https://gitignore.io/api/${url.pathname}`);
 		const text = await response.text();
 		return new Response(text);
 	},
